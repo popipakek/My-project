552ef30b761bd85097cf649e1bbd2311bfa44c56
popipakek <149866448+popipakek@users.noreply.github.com>
2023-11-04 18:22:39 +1000

    Add files via upload

---
+++ Helloworld.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Вывод в консоль привет мир
+        print("Hello World!");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
